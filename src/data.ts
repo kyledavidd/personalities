a0759fdedeba9d0dcf1bec8ded948ab3d78a55fb
kyledavidd <158179197+kyledavidd@users.noreply.github.com>
2025-02-23 01:01:51 +0800

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,51 +1,51 @@
-export const skillList = [{
-  thing: 'Being on Time',
-  detail: 'Many people have a habit of being late to meetings, work and appointments. For most people, time is money and they hate for their time to be wasted. Being on time takes zero talent and shows discipline. It gives a good first impression.',
-  url: 'https://www.shutterstock.com/image-illustration/background-concept-wordcloud-illustration-punctuality-260nw-307867796.jpg',
-  alt: 'Being on Time'
-}, {
-  thing: 'Work Ethic',
-  detail: 'Cutting corners may seem like the easy way out but it always leads to dead-ends. Doing your work with honesty and dedication will take you to new heights. Take pride in the quality of the work you’re doing.',
-  url: 'https://i0.wp.com/plopdo.com/wp-content/uploads/2020/02/Work-Ethics.jpg',
-  alt: 'Work Ethic'
-}, {
-  thing: 'Effort',
-  detail: 'Give your everything when you do something. You shouldn’t regret afterwards that you didn’t make enough effort to make things happen.Always do your best.',
-  url: 'https://thumbs.dreamstime.com/z/hardwork-effort-word-cloud-collection-key-terms-related-to-perseverance-dedication-discipline-motivation-achieving-360534212.jpg',
-  alt: 'Effort'
-}, {
-  thing: 'Body Language',
-  detail: 'Your body language speaks volumes about you. Be aware of your body language and make sure it is saying the message you want to communicate.',
-  url: 'https://www.shutterstock.com/image-illustration/body-language-word-cloud-concept-600w-589871540.jpg',
-  alt: 'Body Language'
-}, {
-  thing: 'Energy',
-  detail: 'There can be days in which our energy levels are low, we don’t feel motivated and we don’t want to get out bed. Never go my feelings. Stay focused. Focus on your goals. If you know your “Why” it will be easier to stay motivated.',
-  url: 'https://st2.depositphotos.com/4428871/6687/v/950/depositphotos_66873491-stock-illustration-energy-word-cloud.jpg',
-  alt: 'Energy'
-}, {
-  thing: 'Passion',
-  detail: 'Cutting corners may seem like the easy way out but it always leads to dead-ends. Doing your work with honesty and dedication will take you to new heights. Take pride in the quality of the work you’re doing.',
-  url: 'https://thumbs.dreamstime.com/b/word-cloud-passion-concept-332679466.jpg',
-  alt: 'Passion'
-}, {
-  thing: 'Attitude',
-  detail: 'The enthusiasm you show when you are passionate about something is easily noticeable and can rub off on others. Being passionate about something also gives you the ability to endure in times when things may not be going your way.',
-  url: 'https://c8.alamy.com/comp/K95HF8/positive-attitude-word-cloud-K95HF8.jpg',
-  alt: 'Attitude'
-}, {
-  thing: 'Being Coachable',
-  detail: 'Be always open to learning. You can never know everything. To increase your chances of success, you need to be a continuous learner.',
-  url: 'https://c8.alamy.com/comp/K1F83D/training-word-cloud-concept-on-white-K1F83D.jpg',
-  alt: 'Being Coachable'
-}, {
-  thing: 'Doing Extra',
-  detail: 'Doing something extra will not cost you much but it will definitely add a lot to your character and make you stand above the crowd.',
-  url: 'https://www.shutterstock.com/shutterstock/photos/369296624/display_1500/stock-vector-expectations-word-cloud-concept-vector-illustration-369296624.jpg',
-  alt: 'Doing Extra'
-}, {
-  thing: 'Being Prepared',
-  detail: '“Failing to plan is planning to fail”. Always be prepared, formulate a plan before you do something. Being prepared reduces significant amount of possibilities of failure.',
-  url: 'https://www.shutterstock.com/image-illustration/be-prepared-word-cloud-concept-260nw-512209684.jpg',
-  alt: 'Being Prepared'
- }];
+export const goatList = [{
+    name: 'Kevin Durant',
+    description: 'Arguably the best scorer in NBA history, Durant combines elite shooting, size, and skills to dominate offensively. A two-time NBA champion, two-time Finals MVP, and former MVP, his versatility and ability to score from anywhere make him one of the most unstoppable forces in the game.',
+    url: 'https://s.yimg.com/ny/api/res/1.2/ti0SOoForNaOHAjOtgbrnQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyNDI7aD04Mjg-/https://media.zenfs.com/en/azcentral-the-arizona-republic/6ffe4f72769e2eb32eb3e3de02e8e70f',
+    alt: 'easymoneysniper'
+  }, {
+    name: 'Lebron James',
+    description: ' A basketball legend and one of the greatest players of all time, LeBron is known for his basketball IQ, leadership, and all-around game. With four NBA championships and four MVPs, he has redefined longevity and consistency while excelling as both a scorer and a playmaker.',
+    url: 'https://i.pinimg.com/736x/6d/26/a2/6d26a28e9269843a0103da816b83457f.jpg',
+    alt: 'KingJames'
+  }, {
+    name: 'Stephen Curry',
+    description: ' Revolutionized the game with his extraordinary shooting range, Curry is the greatest shooter in NBA history. A two-time MVP and three-time NBA champion, he has transformed the way the game is played with his deep threes and impeccable ball-handling, leading the Golden State Warriors to multiple titles.',
+    url: 'https://wallpapersok.com/images/hd/steph-curry-with-night-night-pose-cll2yrmy8e177yig.jpg',
+    alt: 'Chef Curry'
+  }, {
+    name: 'Michael Jordan',
+    description: 'Often regarded as the greatest player in basketball history, Jordan’s six NBA championships, five MVP awards, and ten scoring titles stand as a testament to his dominance. Known for his fierce competitiveness and clutch performances, he elevated the Chicago Bulls to global prominence in the 1990s.',
+    url: 'https://upload.wikimedia.org/wikipedia/en/c/c7/Michael_Jordan_crying.jpg',
+    alt: 'MJ'
+  }, {
+    name: 'Devin Booker',
+    description: 'A prolific scorer with a smooth shooting stroke, Booker emerged as one of the NBA’s brightest stars. A two-time All-Star and the leader of the Phoenix Suns, Booker played a pivotal role in leading his team to the NBA Finals in 2021, showcasing his ability to deliver under pressure.',
+    url: 'https://s.yimg.com/ny/api/res/1.2/ow1gmKI.rnDvng4Rdz9OJw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQzOQ--/https://s.yimg.com/os/creatr-uploaded-images/2024-12/76f26fe0-c265-11ef-97be-acd4d32cc552',
+    alt: 'DBook'
+  }, {
+    name: 'Derrick Rose',
+    description: 'A former MVP who captured the hearts of basketball fans worldwide, Rose was known for his explosive athleticism, incredible speed, and finishing ability. Although injuries hampered his career, Rose’s resilience and ability to continue contributing at a high level have earned him deep respect.',
+    url: 'https://ontapsportsnet.com/.image/t_share/MTk3ODc3NDcyMzQyMTI0NDA2/derrick-rose-chicago-bulls-jersey.jpg',
+    alt: 'Drose'
+  }, {
+    name: 'Kyrie Irving',
+    description: " Known for his dazzling ball-handling and clutch shot-making, Irving is one of the most skilled point guards in NBA history. A six-time All-Star and NBA champion, his ability to break down defenses and hit big shots, including his iconic game-winner in the 2016 Finals, has cemented his legacy as a game-changer.",
+    url: 'https://dims.apnews.com/dims4/default/e31ee9e/2147483647/strip/true/crop/3548x2366+0+0/resize/599x399!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F32%2F52%2F8298ceb57fc68f562fe0fb787c92%2Fd5b925c805e74f56a0a41fb9a37a2881',
+    alt: 'Kai11'
+  }, {
+    name: 'Luka Doncic',
+    description: "A generational talent, Luka has taken the NBA by storm with his maturity, court vision, and scoring ability at just 24 years old. With a combination of size, skills, and an elite basketball IQ, Luka is one of the brightest young stars and a future MVP contender for years to come.",
+    url: 'https://images.foxtv.com/static.fox4news.com/www.fox4news.com/content/uploads/2025/02/932/524/gettyimages-2197306636-scaled-e1739215497574.jpg?ve=1&tl=1',
+    alt: 'LukaMagic'
+  }, {
+    name: 'Nikola Jokic',
+    description: 'The most unique big man in the NBA, Jokić is a two-time MVP and a masterful passer. His exceptional basketball IQ, combined with his ability to score, rebound, and facilitate at an elite level, has made him one of the best all-around players in the league, redefining the center position.',
+    url: 'https://static01.nyt.com/athletic/uploads/wp/2023/10/07085025/231007-Nikola-Jokic-scaled-e1696683052376.jpg',
+    alt: 'Joker'
+  }, {
+    name: 'James Harden',
+    description: 'Known for his offensive mastery, Harden is a three-time scoring champion and former MVP. His step-back three, elite passing, and crafty finishing make him one of the most dangerous offensive threats in NBA history. Harden has led teams to multiple deep playoff runs with his dynamic playmaking.',
+    url: 'https://library.sportingnews.com/styles/crop_style_16_9_desktop/s3/2023-12/James%20Harden%20121923.jpeg?h=920929c4&itok=BHTNzoBw',
+    alt: 'JH'
+  }];
